fa86af41099525aae1aaebfdd79c3fef4f889a84
jpgarrett <jpgarrett24@gmail.com>
2020-07-08 21:46:17 -0700

    Putting it together assignment. Added a button to increase the age of each class bases component using state

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: this.props.firstName,
+            lastName: this.props.lastName,
+            age: this.props.age,
+            hairColor: this.props.hairColor,
+        }
+    }
+
     render() {
         return (
-            <section>
-                <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                <p>Age: {this.props.age}</p>
-                <p>Hair Color: {this.props.hairColor}</p>
-            </section>
+            <>
+                <section>
+                    <h1>{this.state.lastName}, {this.state.firstName}</h1>
+                    <p>Age: {this.state.age}</p>
+                    <p>Hair Color: {this.state.hairColor}</p>
+                </section>
+                <button onClick={() => this.setState({ age: this.state.age += 1 })}>Birthday Button For {this.state.firstName} {this.state.lastName}</button>
+            </>
         )
     }
 }
